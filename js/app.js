db2aff6309c6013d935e65386c312bd8cc4b0804
Adeleke <adeleketogun@gmail.com>
2014-09-08 18:29:04 +0100

    With object literal & other bull

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
-//Assign text box input to variable input
-var item = $(".user_input");
-//Assign add item button to variable addItem
-var addItem = $(".addItem");
-
-// var newItem = item.val();
-
 var acts = {
+
+	item:null,
+	addItem:null,
+	wishlist:null,
+	purchased:null,
 	//create a function that adds the new entry into the list
-	addtoList: function(){
+	addtoList: function(e){
 		//Assign the value of the item to a variable newItem
-		var newItem = item.val();
+		var newItem = acts.item.val();
+		e.preventDefault();
 		//Put the item and tags into the variable content
 		var content = "<li>" + "<input type='checkbox'/>" + "<label>" + newItem + "</label>" + "</li>"; 
 		console.log(content);
 		// $(content).appendTo(".wishlist ul");
 		$(".wishList ul").append(content)
+		acts.item.val("");
 	},
-	//Request submit button to add Item when clicked
-	submitAction: function(){
-		addItem.click(function(event) {
-			event.preventDefault();
-			acts.addtoList();
-			console.log("clicked");
-		})
-	},
-
-
 
 	//create a function that initializes actions
 	initialize: function(){
-		acts.submitAction();
-			console.log("clicked");
+		//Assign add item button to variable addItem
+		acts.addItem = $(".addItem");
+		//Assign text box input to variable input
+		acts.item = $(".user_input");
+		//Assign the wishlist to a variable
+		acts.wishlist = $(".wishList ul")
+		//Assign the purchased list to a variable
+		acts.purchased = $(".purchased ul")
+		acts.addItem.click(acts.addtoList);	
+			
+
 	}
 }
 
-$(document).ready(acts.initialize());
+$(document).ready(acts.initialize);
 
